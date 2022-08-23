abd296441ea67c6640a9485da18a06bfd152d67b
Bex <ruthrebekahcupido@gmail.com>
2022-08-23 10:10:05 -0700

    Print posibble combinations

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for number in range(0, 100):
+    if number / 10 < number % 10 and number / 10 != number % 10:
+        print("{:0>2d}".format(number), end="")
+    else:
+        continue
+    if number < 89:
+            print(", ", end="")
+    else:
+        print()
