946a2652fc189a19acfec208823318543ee4cfb4
Bex <ruthrebekahcupido@gmail.com>
2022-08-27 15:08:08 -0700

    debugging

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,7 +2,7 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
-    for counter in range(len(my_list)):
-        if counter == idx:
-            my_list[counter] = element
+    for index in range(len(my_list)):
+        if index == idx:
+            my_list[index] = element
     return my_list
