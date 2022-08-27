2399a78cf1f22fcb22b588833b24193211f4fcae
Bex <ruthrebekahcupido@gmail.com>
2022-08-27 15:03:18 -0700

    Replace element fix error

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    counter = 0
-    for int in my_list:
+    if idx < 0:
+        return my_list
+    for counter in range(len(my_list)):
         if counter == idx:
             my_list[counter] = element
-            return (my_list)
-        counter += 1
-        return (my_list)
+    return my_list
