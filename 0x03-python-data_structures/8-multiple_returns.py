0df71bfd15f73785b80248d36ebfbcbf77b14ba1
Bex <ruthrebekahcupido@gmail.com>
2022-08-28 10:40:45 -0700

    Pycode

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence:
-        first_lengthchar= (len(sentence), sentence[0])
+        first_lengthchar = (len(sentence), sentence[0])
     else:
         first_lengthchar = (0, None)
     return first_lengthchar
