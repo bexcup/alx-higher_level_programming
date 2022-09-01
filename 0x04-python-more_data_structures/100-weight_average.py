58a8ac19688a84326c5aec301d3486f61b427361
Bex <ruthrebekahcupido@gmail.com>
2022-09-01 06:08:07 -0700

    Weighted average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    divid = 0
+    divis = 0
+    if not my_list:
+        return 0
+    for i in my_list:
+        divid += (i[0] * i[1])
+        divis += i[1]
+    return divid / divis 
