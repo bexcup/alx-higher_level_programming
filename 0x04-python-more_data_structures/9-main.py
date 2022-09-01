31501c5c845c7fbf1f41fe5e129b6e2ff08978df
Bex <ruthrebekahcupido@gmail.com>
2022-09-01 05:14:02 -0700

    Main test files

--- 0x04-python-more_data_structures/9-main.py
+++ 0x04-python-more_data_structures/9-main.py
@@ -8,16 +8,3 @@ new_dict = multiply_by_2(a_dictionary)
 print_sorted_dictionary(a_dictionary)
 print("--")
 print_sorted_dictionary(new_dict)
-
-guillaume@ubuntu:~/0x04$ ./9-main.py
-Alex: 8
-Bob: 14
-John: 12
-Mike: 14
-Molly: 16
---
-Alex: 16
-Bob: 28
-John: 24
-Mike: 28
-Molly: 32
